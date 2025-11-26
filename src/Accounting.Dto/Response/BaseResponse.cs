44e165f8b053e79e6ddea98fd190c134f6259a44
ahernandez <ahernandez>
2025-11-26 10:10:17 -0600

    Agregar archivos de proyecto.

---
+++ src/Accounting.Dto/Response/BaseResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace accounting.Dto.Response
+{
+    public class BaseResponse
+    {
+
+        public bool Success { get; set; }
+        public string? ErrorMessage { get; set; }
+
+
+    }
+}
