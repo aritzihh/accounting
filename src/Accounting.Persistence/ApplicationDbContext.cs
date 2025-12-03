baeda11a9d67a4b78f5e652405e5ff00bc793547
JAriza <jariza@grupointeract.com.mx>
2025-12-03 17:29:56 -0600

    Agregar contexto y configuración de entidades

---
+++ src/Accounting.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Accounting.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        }
+
+
+    }
+}
