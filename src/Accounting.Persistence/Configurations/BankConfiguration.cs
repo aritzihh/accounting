6dcb93b4f92fa98cc8daee3c8f3359a24852eed8
JAriza <jariza@grupointeract.com.mx>
2025-12-03 17:28:13 -0600

    Agregar archivos de configuración para dos entidades (banks - satcountrydiot)

---
+++ src/Accounting.Persistence/Configurations/BankConfiguration.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accounting.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Accounting.Persistence.Configurations
+{
+    public class BankConfiguration : IEntityTypeConfiguration<Bank>
+    {
+
+        public void Configure(EntityTypeBuilder<Bank> builder)
+        {
+            builder.ToTable("banks", "dbo");
+
+            builder.HasKey(e => e.BankId);
+
+            builder.Property(e => e.BankId)
+                .HasColumnName("bank_id")
+                .ValueGeneratedOnAdd();
+
+            builder.Property(e => e.Name)
+                .IsRequired()
+                .HasColumnName("name")
+                .HasMaxLength(150);
+
+            builder.Property(e => e.BankCode)
+                .HasColumnName("bank_code")
+                .HasMaxLength(40);
+
+            builder.Property(e => e.CreatedAt)
+                .IsRequired()
+                .HasColumnName("created_at")
+                .HasDefaultValueSql("dateadd(hour, -6, GETUTCDATE())")
+                .ValueGeneratedOnAdd();
+
+            builder.Property(e => e.UpdatedAt)
+                .HasColumnName("updated_at");
+
+            builder.Property(e => e.IsActive)
+                .IsRequired()
+                .HasColumnName("is_active")
+                .HasDefaultValue(true);
+
+            builder.HasIndex(e => e.Name)
+                .HasDatabaseName("IX_bank_name");
+
+            builder.HasIndex(e => e.BankCode)
+                .HasDatabaseName("IX_bank_bank_code");
+
+            builder.HasIndex(e => e.IsActive)
+                .HasDatabaseName("IX_bank_is_active");
+
+            builder.HasIndex(e => new { e.IsActive, e.Name })
+                .HasDatabaseName("IX_bank_is_active_name");
+        }
+
+    }
+}
---
+++ src/Accounting.Persistence/Configurations/SatCountryDiotConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Accounting.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Accounting.Persistence.Configurations
+{
+    public class SatCountryDiotConfiguration : IEntityTypeConfiguration<SatCountryDiot>
+    {
+
+        public void Configure(EntityTypeBuilder<SatCountryDiot> builder)
+        {
+
+            builder.ToTable("sat_country_diot");         
+
+            builder.HasKey(e => e.SatCountryId);
+
+            builder.Property(e => e.SatCountryId)
+                .HasColumnName("sat_country_id")
+                .ValueGeneratedOnAdd();
+
+            builder.Property(e => e.KeyCountry)
+                .IsRequired()
+                .HasColumnName("key_country")
+                .HasMaxLength(10);
+
+            builder.Property(e => e.Description)
+                .IsRequired()
+                .HasColumnName("description")
+                .HasMaxLength(250);
+
+            builder.Property(e => e.IsActive)
+                .IsRequired()
+                .HasColumnName("is_active")
+                .HasDefaultValue(true);
+
+            builder.HasIndex(e => e.KeyCountry)
+                .IsUnique()
+                .HasDatabaseName("IX_sat_country_diot_key_country");
+
+            builder.HasIndex(e => e.IsActive)
+                .HasDatabaseName("IX_sat_country_diot_is_active");
+
+        }
+
+    }
+}
